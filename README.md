e779523c2fa15e9b27d1f9286d3fef975a5b1694
penalozajohn25 <johnmanuelpenaloza@gmail.com>
2019-08-07 02:38:21 +0000

    voz texto

---
+++ README.md
@@ -0,0 +1,16 @@
+# textToSpeech application
+
+## description 🤓
+
+Ionic application speak the text. When using the Text To Speech plugin.
+
+
+### Installation 🔧
+* ionic cordova platform add android
+* ionic cordova build android. 
+
+## Deployment 📦
+
+```
+ search apk in /platforms/android/app/build/outputs/apk/debug/app-debug.apk
+```
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+ 
+import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
-
+  text_sentences = [];
+ 
+  constructor(
+    private tts: TextToSpeech
+  ) {
+    this.text_sentences = [
+      "There is no such thing as fun for the whole family",
+      "Hola como estas mundo ?.",
+      "lo has logrado tio"
+    ]
+  }
+ 
+ 
+  textToSpeech(text) {
+    this.tts.speak(text)
+      .then(() => console.log('Success'))
+      .catch((reason: any) => console.log(reason));
+  }
+ 
+ 
 }
